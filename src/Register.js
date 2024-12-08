902b97094c0dc73c89ace97889f614d9e1b0c907
C-Venkata-Sankar <108678736+C-Venkata-Sankar@users.noreply.github.com>
2024-12-08 18:32:18 +0530

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -25,7 +25,7 @@ function Register() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/register", {
+      const response = await fetch("https://deccan-destinations-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
