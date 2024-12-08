38cb892e1d72955bcee563a1af043d965d9a0410
C-Venkata-Sankar <108678736+C-Venkata-Sankar@users.noreply.github.com>
2024-12-08 18:31:46 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -17,7 +17,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://deccan-destinations-backend.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
